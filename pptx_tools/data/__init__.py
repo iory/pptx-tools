bfa14e295c74e0b3ee49164524ae8f46fadb3054
iory <ab.ioryz@gmail.com>
2022-05-10 14:53:52 +0900

    Add sample pptx

--- pptx_tools/data/__init__.py
+++ pptx_tools/data/__init__.py
@@ -6,3 +6,7 @@ data_dir = Path(__file__).resolve().parent
 
 def get_transparent_img_path():
     return data_dir / 'transparent.png'
+
+
+def get_hello_pptx_path():
+    return data_dir / 'hello.pptx'
